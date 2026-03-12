28131759b5ccde713842dd567058e4030d78a810
Jostein Tysse <jostein.h.tysse@gmail.com>
2026-03-12 10:24:18 +0100

    Add order service

--- backend/src/services/order.ts
+++ backend/src/services/order.ts
@@ -1,4 +1,66 @@
 import EventClient from "../clients/eventClient";
-import { StoreEventTypes } from "../events";
+import { CartEventTypes, StoreEventTypes } from "../events";
+import { Order, OrderItem, PopularItem } from "../types";
+import { getCart } from "./cart";
 
-// Lykke til 🚀
+const getStreamName = (orderId: string): string => `order-${orderId}`;
+
+export async function createOrder(client: EventClient, cartId: string) {
+  const cart = await getCart(client, cartId);
+  if (!cart) {
+    throw new Error(`Cart not found with id: ${cartId}`);
+  }
+  const orderId = crypto.randomUUID();
+  const events: StoreEventTypes[] = [
+    {
+      type: CartEventTypes.OrderCreated,
+      subject: orderId,
+      items: cart.items,
+      totalAmount: cart.total,
+    },
+  ];
+  await client.emit(getStreamName(orderId), events);
+}
+
+export async function getRecentOrders(client: EventClient, limit: number) {
+  const events = await client.read("$ce-order");
+  const orders: Order[] = [];
+  for await (const event of events) {
+    if (event.type === CartEventTypes.OrderCreated) {
+      orders.unshift({
+        orderId: event.subject,
+        totalAmount: event.totalAmount,
+        items: event.items.map((item) => ({
+          productId: item.productId,
+          productName: item.productName,
+          productPrice: item.productPrice,
+        })),
+        createdAt: event.createdAt,
+      });
+    }
+  }
+  return orders.slice(0, limit);
+}
+
+export async function getPopularItems(client: EventClient, limit: number) {
+  const events = await client.read("$ce-order");
+  const itemMap: Map<string, PopularItem> = new Map();
+  for await (const event of events) {
+    if (event.type === CartEventTypes.OrderCreated) {
+      event.items.forEach((item) => {
+        if (itemMap.has(item.productId)) {
+          itemMap.get(item.productId)!.totalQuantity += 1;
+        } else {
+          itemMap.set(item.productId, {
+            productId: item.productId,
+            productName: item.productName,
+            totalQuantity: 1,
+          });
+        }
+      });
+    }
+  }
+  return Array.from(itemMap.values())
+    .sort((a, b) => b.totalQuantity - a.totalQuantity)
+    .slice(0, limit);
+}
